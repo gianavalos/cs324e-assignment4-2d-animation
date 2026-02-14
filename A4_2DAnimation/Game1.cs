4f76706fdba94b853375872391626a18cfddb88d
gianavalos <gianfco@icloud.com>
2026-02-13 23:23:40 -0600

    Add Working tiger animation with hierarchical transformations

--- A4_2DAnimation/Game1.cs
+++ A4_2DAnimation/Game1.cs
@@ -9,6 +9,16 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    private Texture2D _background;
+    
+    private Texture2D _tigerBody;
+    private Texture2D _tigerFrontLeg;
+    private Texture2D _tigerBackLeg;
+    private Texture2D _TigerTail;
+    
+    private Tiger _tiger1;
+    private Tiger _tiger2;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -26,8 +36,40 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        _background = Content.Load<Texture2D>("imgs/rainforest");
 
-        // TODO: use this.Content to load your game content here
+        _tigerBody = Content.Load<Texture2D>("Tiger/tiger-body");
+        _tigerFrontLeg = Content.Load<Texture2D>("Tiger/tiger-front-leg");
+        _tigerBackLeg = Content.Load<Texture2D>("Tiger/tiger-back-leg");
+        _TigerTail = Content.Load<Texture2D>("Tiger/tiger-tail");
+
+
+        _tiger1 = new Tiger(
+            _tigerBody,
+            _tigerFrontLeg,
+            _tigerBackLeg,
+            _TigerTail,
+            new Vector2(200, 350),
+            Color.White,
+            0.33f,
+            2.3f,
+            _graphics.PreferredBackBufferWidth,
+            _graphics.PreferredBackBufferHeight);
+        
+        _tiger2 = new Tiger(
+            _tigerBody,
+            _tigerFrontLeg,
+            _tigerBackLeg,
+            _TigerTail,
+            new Vector2(400, 325),
+            Color.Orange,
+            0.2f,
+            -1.5f,
+            _graphics.PreferredBackBufferWidth,
+            _graphics.PreferredBackBufferHeight);
+
+        
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,8 +77,9 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // TODO: Add your update logic here
+       
+        _tiger1.Update(gameTime);
+        _tiger2.Update(gameTime);
 
         base.Update(gameTime);
     }
@@ -44,8 +87,19 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
+        
+        _spriteBatch.Begin();
+        
+        _spriteBatch.Draw(_background,
+                        new Rectangle(0, 0,
+                            _graphics.PreferredBackBufferWidth,
+                            _graphics.PreferredBackBufferHeight),
+                        Color.White);
+        _spriteBatch.End();
+        
+        _tiger2.Draw(_spriteBatch);
+        _tiger1.Draw(_spriteBatch);
+        
 
         base.Draw(gameTime);
     }
---
+++ A4_2DAnimation/Tiger.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+namespace A4_2DAnimation;
+
+public class Tiger
+{
+    private Texture2D _bodyTexture;
+    private Texture2D _frontLegTexture;
+    private Texture2D _backLegTexture;
+    private Texture2D _tailTexture;
+    
+    private Vector2 _position;
+    private Color _tint;
+    private float _scale;
+    private float _walkSpeed;
+    private float _walkDirection = 1;
+
+    private float _currentScale;
+    private float _legRotation;
+    private  float _tailRotation;
+    private float _elapsedTime;
+    
+    private int _screenWidth;
+    private int _screenHeight;
+
+    public Tiger(Texture2D body,
+        Texture2D frontLeg,
+        Texture2D backLeg,
+        Texture2D tail,
+        Vector2 position,
+        Color tint,
+        float scale,
+        float walkSpeed,
+        int screenWidth,
+        int screenHeight)
+    {
+        _bodyTexture = body;
+        _frontLegTexture = frontLeg;
+        _backLegTexture = backLeg;
+        _tailTexture = tail;
+        _position = position;
+        _tint = tint;
+        _scale = scale;
+        _walkSpeed = walkSpeed;
+        _screenWidth = screenWidth;
+        _screenHeight = screenHeight;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        _elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+        
+        _position.X += _walkDirection * _walkSpeed;
+
+        if (_position.X > _screenWidth - 100 || _position.X < 100)
+        {
+            _walkDirection *= -1;
+        }
+        
+        _currentScale = _scale + (float)Math.Sin(_elapsedTime) * 0.05f;
+        
+        _legRotation = (float)Math.Sin(_elapsedTime * _walkSpeed * 5) * 0.05f;
+        
+        _tailRotation = (float)Math.Sin(_elapsedTime * 3) * 0.05f;
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        Matrix rootMatrix = Matrix.CreateScale(_currentScale) * 
+                            Matrix.CreateTranslation(_position.X, _position.Y, 0);
+        
+        
+        spriteBatch.Begin(transformMatrix: rootMatrix);
+        
+        Vector2 frontLeftPosition = new Vector2(10, 20);
+        Vector2 legOrigin = new Vector2(_bodyTexture.Width / 2, _bodyTexture.Height / 2);
+        spriteBatch.Draw(_frontLegTexture,
+            frontLeftPosition,
+            null,
+            _tint,
+            -_legRotation,
+            legOrigin,
+            1.0f,
+            SpriteEffects.None,
+            0.0f);
+        
+        Vector2 backLeftPos = new Vector2(30, 20);
+        Vector2 backLegOrigin = new Vector2(_bodyTexture.Width / 2, _bodyTexture.Height / 2);
+        spriteBatch.Draw(_backLegTexture,
+            backLeftPos,
+            null,
+            _tint,
+            -_legRotation,
+            backLegOrigin,
+            1.0f,
+            SpriteEffects.None,
+            0.0f);
+        
+        Vector2 bodyOrigin = new Vector2(_bodyTexture.Width / 2, _bodyTexture.Height / 2);
+        spriteBatch.Draw(_bodyTexture,
+                        Vector2.Zero,
+                        null,
+                        _tint,
+                        0.0f,
+                        bodyOrigin,
+                        1.0f,
+                        SpriteEffects.None,
+                        0.0f);
+        
+
+        
+        Vector2 frontRightPosition = new Vector2(-10, 20);
+        spriteBatch.Draw(_frontLegTexture,
+                        frontRightPosition,
+                        null,
+                        _tint,
+                        -_legRotation,
+                        legOrigin,
+                        1.0f,
+                        SpriteEffects.None,
+                        0.0f);
+        
+        
+        Vector2 backRightPos = new Vector2(0, 30);
+        spriteBatch.Draw(_backLegTexture,
+            backRightPos,
+            null,
+            _tint,
+            _legRotation,
+            backLegOrigin,
+            1.0f,
+            SpriteEffects.None,
+            0.0f);
+        
+        Vector2 tailPos = new Vector2(-760, 7);
+        Vector2 tailOrigin = new Vector2(0, _tailTexture.Height / 2);
+        spriteBatch.Draw(_tailTexture,
+            tailPos,
+            null,
+            _tint,
+            _tailRotation,
+            tailOrigin,
+            1.0f,
+            SpriteEffects.None,
+            0.0f);
+        
+        spriteBatch.End();
+    }
+}
